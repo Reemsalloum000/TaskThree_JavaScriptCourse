392061c86f9190bf7505a68de623f14c4db81cb6
Reem Salloum <1202160@student.birzeit.edu>
2025-03-15 21:58:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function modifyContent() {
+    // Change the heading text
+    document.getElementById("heading").innerText = "Updated Heading via JavaScript";
+
+    // Add a new class to change paragraph styling and div background
+    document.getElementById("content-box").classList.add("modified");
+}
